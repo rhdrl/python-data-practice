677f74e5b8336366858d4ffb478a881f728b417d
rhdrl <pgy2040807@gmail.com>
2026-09-16 10:32:28 +0900

    이젠 온도만 물어보지 않아..

--- array.py
+++ array.py
@@ -13,3 +13,12 @@ print(temp[2:])
 temp.append(24.5)
 temp[0] = 20.0
 print(temp)
+
+year = 2026
+city = "인천"
+value = [10, 20, 30, 40]
+
+print(type(year))
+print(type(city))
+print(type(value))
+print(len(value))
