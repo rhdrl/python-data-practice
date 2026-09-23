381555d9885a68776fd8c2afdd147c64c07f4e74
rhdrl <pgy2040807@gmail.com>
2026-09-23 11:02:18 +0900

    list 수정

---
+++ list.py
@@ -0,0 +1,20 @@
+while True:
+    print("\n===== 메뉴 =====")
+    print("1. 데이터 입력")
+    print("2. 데이터 보기")
+    print("3. 평균")
+    print("0. 종료")
+
+    menu = input("메뉴 선택: ")
+
+    if menu == "1": 
+    elif menu == "2":
+        print("데이터 보기")
+    elif menu == "3":
+        print("평균 계산")
+    elif menu == "0":
+        print("프로그램 종료")
+        break
+    else:
+        print("잘못된 메뉴 입니다.")
+
